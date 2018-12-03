6d7bdd88f756838715e6c092994711c51a80e40d
Christopher Kistner <chris.kistner0@gmail.com>
2018-12-03 14:00:43 -0800

    Basic front end directory setup and structured

---
+++ bundle.js
@@ -0,0 +1,31 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const index = require('./src/index');
+const dashboard = require('./src/dashboard');
+const library = require('./src/library');
+const libdetails = require('./src/libdetails')
+
+const pageInitialization = {
+  '/' : index.init,
+  '/index.html': index.init,
+  '/dashboard.html': dashboard.init,
+  '/library.html': library.init,
+  '/libdetails.html': libdetails.init
+}
+
+const path = window.location.pathname
+ 
+if(pageInitialization.hasOwnProperty(path)) {
+  pageInitialization[path]()
+}
+else {
+  console.error(`${path} does not have an initializer`)
+}
+},{"./src/dashboard":2,"./src/index":3,"./src/libdetails":4,"./src/library":5}],2:[function(require,module,exports){
+
+},{}],3:[function(require,module,exports){
+arguments[4][2][0].apply(exports,arguments)
+},{"dup":2}],4:[function(require,module,exports){
+arguments[4][2][0].apply(exports,arguments)
+},{"dup":2}],5:[function(require,module,exports){
+arguments[4][2][0].apply(exports,arguments)
+},{"dup":2}]},{},[1]);
---
+++ main.js
@@ -0,0 +1,21 @@
+const index = require('./src/index');
+const dashboard = require('./src/dashboard');
+const library = require('./src/library');
+const libdetails = require('./src/libdetails')
+
+const pageInitialization = {
+  '/' : index.init,
+  '/index.html': index.init,
+  '/dashboard.html': dashboard.init,
+  '/library.html': library.init,
+  '/libdetails.html': libdetails.init
+}
+
+const path = window.location.pathname
+ 
+if(pageInitialization.hasOwnProperty(path)) {
+  pageInitialization[path]()
+}
+else {
+  console.error(`${path} does not have an initializer`)
+}
