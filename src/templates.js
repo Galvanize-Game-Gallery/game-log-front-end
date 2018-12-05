97379d73485a72aa4e87bbe48699be19bae9df6e
Christopher Kistner <chris.kistner0@gmail.com>
2018-12-04 16:56:32 -0800

    Added button to each row to add game

--- src/templates.js
+++ src/templates.js
@@ -1,21 +1,20 @@
 const publicGameRow = (id, coverArt, title, desc) => {
-    console.log(coverArt);
     return `
     <div class="row justify-content-start item-rows border public-game-rows" data-id="${id}">
-        <div class="col-4">
-            <img src="${coverArt}" height="150px">
+        <div class="col-3 border">
+            <img src="${coverArt}" height="150px" width="125">
         </div>
-        <div class="col-8">
+        <div class="col-9">
             <div class="row justify-content-start">
                 <div class="col-8">
                     <h2>${title}</h2>
                 </div>
                 <div class="col-4">
-                    <ul id="actor-list" data-id=${id}> </ul>
+                    <button id="edit-film" data-id="${id}" type="button" class="btn btn-success">Add</button>
                 </div>
                 <div class="row justify-content-center">
                     <div class="col">
-                        <p>${desc}</p>
+                        <p class="public-game-rows-text">${desc}</p>
                     </div>
                 </div>
             </div>
