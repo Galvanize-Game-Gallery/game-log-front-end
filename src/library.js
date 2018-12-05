08b79cc4de45fd611298f13f6f775a7dcbf3837a
Christopher Kistner <chris.kistner0@gmail.com>
2018-12-05 11:45:57 -0800

    Library game rows are now pulling from the database and rendering properly

--- src/library.js
+++ src/library.js
@@ -1,21 +1,16 @@
 function init() {
-
     const create = require('./templates');
+    const game = "http://localhost:3000/game";
+    const axios = require('axios');
 
-    const testArr = [
-        {
-        igdb_id: 41608,
-        title: 'Halo: The Master Chief Collection',
-        cover_url: '//images.igdb.com/igdb/image/upload/t_thumb/kuaub4pjaajuepmlxuq3.jpg',
-        desc: 'Relive the greatest adventures of the ultimate solder with Halo: The Master Chief Collection, which includes the complete story of the Master Chief on one console for the first time ever. Halo: Combat Evolved , Halo 2, Halo 3 and Halo 4 are all packed into this massive package. And not only is the debut Halo delivered in its acclaimed Anniversary package, but for the first time, an entirely remastered version of the iconic sequel that revolutionized online console gaming is available with the Halo 2 Anniversary version of the game. This collection lets you experience Halo in an all-new way, at higher visual fidelity running at 60 frames per second (fps) on dedicated servers. \n \nHalo: The Master Chief Collection features curated campaign and multiplayer playlists across all four games and features more than 100 multiplayer maps -- authentic to their original release. In addition, Halo 2 Anniversary includes six all-new maps built from the ground up for Xbox One.'
-    },
-    {
-        igdb_id: 25076, 
-        title: 'Red Dead Redemption 2', 
-        cover_url: '//images.igdb.com/igdb/image/upload/t_thumb/yfk9f2lbo0r7slytuhra.jpg' ,
-        desc: 'Developed by the creators of Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale of life in America’s unforgiving heartland. The game\'s vast and atmospheric world will also provide the foundation for a brand new online multiplayer experience.' 
-    }
-];
+    function getlibrary() {
+        axios.get(game + '/library')
+        .then(function (result) {
+            let library = result.data;
+            generateGames(library);
+        })
+    };
+
 
     function generateGames(arr) {
 
@@ -25,7 +20,7 @@ function init() {
         };
 
         function formatDesc(str) {
-            if(str.length <= 300) {
+            if (str.length <= 300) {
                 return str
             } else {
                 let newStr = str.slice(0, 300)
@@ -35,19 +30,25 @@ function init() {
 
         const appliedTemplates = arr.map(game => create.publicGameRow(game.igdb_id, formatCover(game.cover_url), game.title, formatDesc(game.desc))).join('\n');
         document.querySelector(".main-body").innerHTML = appliedTemplates;
+
+        for (let game of arr) {
+            
+        }
     }
 
-    generateGames(testArr);
-  
-  function addNewEvent(){
+    
 
-    document.getElementById('add-new-game').addEventListener('click', function(e) {
-        e.preventDefault()
-        window.location = '/newlibgame.html'
-    })
+    function addNewEvent() {
+
+        document.getElementById('add-new-game').addEventListener('click', function (e) {
+            e.preventDefault()
+            window.location = '/newlibgame.html'
+        })
+    }
+    getlibrary();
 }
 
-}
+
 module.exports= {
     init,
 }
--- src/templates.js
+++ src/templates.js
@@ -1,16 +1,16 @@
 const publicGameRow = (id, coverArt, title, desc) => {
     return `
     <div class="row justify-content-start item-rows border public-game-rows" data-id="${id}">
-        <div class="col-3 border">
-            <img src="${coverArt}" height="150px" width="125">
+        <div class="col-2">
+            <img class="cover-art" src="${coverArt}" height="150px" width="125">
         </div>
-        <div class="col-9">
+        <div class="col-10">
             <div class="row justify-content-start">
                 <div class="col-8">
                     <h2>${title}</h2>
                 </div>
                 <div class="col-4">
-                    <button id="edit-film" data-id="${id}" type="button" class="btn btn-success">Add</button>
+                    
                 </div>
                 <div class="row justify-content-center">
                     <div class="col">
