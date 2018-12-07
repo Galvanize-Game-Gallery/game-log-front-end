540000db8ec0899e92c0446ac17feee018a81c62
ChrisIhler <Ihlerconsulting@gmail.com>
2018-12-07 08:38:12 -0800

    link to igdb fixed

--- src/library.js
+++ src/library.js
@@ -1,6 +1,6 @@
 function init() {
 
-  let userID = function getUserID(){
+  let userID = function getUserID() {
     let linkID = window.location.search
     let userID = linkID.slice(4)
     console.log('user id', userID)
@@ -12,7 +12,7 @@ function init() {
     let linkID = window.location.search
     let userID = linkID.slice(4)
     dynamicLink = link + linkID
-    nav.setAttribute('href', dynamicLink)  
+    nav.setAttribute('href', dynamicLink)
   }
 
   let navLibrary = document.querySelector('#navDashboard')
@@ -20,74 +20,78 @@ function init() {
   dynamicLink(userLibraryLink, navLibrary)
 
 
-    const create = require('./templates');
-    const game = "https://lit-escarpment-87610.herokuapp.com/game";
-    const axios = require('axios');
+  const create = require('./templates');
+  const game = "https://lit-escarpment-87610.herokuapp.com/game";
+  const axios = require('axios');
 
-    function getlibrary() {
-        axios.get(game + '/library')
-        .then(function (result) {
-            let library = result.data;
-            generateGames(library);
-        })
+  function getlibrary() {
+    axios.get(game + '/library')
+      .then(function (result) {
+        let library = result.data;
+        generateGames(library);
+      })
+  };
+
+  function generateGames(arr) {
+
+    function formatCover(str) {
+      if (str[0] === '/') {
+        let newStr = str.slice(2);
+        return `http://${newStr}`;
+      } else {
+        return str
+      }
     };
 
-    function generateGames(arr) {
-
-        function formatCover(str) {
-            if(str[0] === '/'){
-             let newStr = str.slice(2);
-             return `http://${newStr}`;
-            }
-            else {
-                return str
-            }
-        };
-
-        function formatDesc(str) {
-            if (str.length <= 300) {
-                return str
-            } else {
-                let newStr = str.slice(0, 300)
-                return `${newStr}...`;
-            }
-        }
-
-        // const headers = create.gameHeaders()
-
-        const appliedTemplates = arr.map(game => create.publicGameRow(game.igdb_id, formatCover(game.cover_url), game.title, formatDesc(game.desc))).join('\n');
-        // document.querySelector(".main-body").innerHTML = headers + appliedTemplates;
-        document.querySelector(".main-body").innerHTML = appliedTemplates;
-
-        for (let game of arr) {
-            const platformList = [];
-            for (let system of game.platforms) {
-                if (system === 'Xbox One') {
-                    platformList.push(create.platformTagXbox(system))
-                } else if (system=== 'Playstation 4') {
-                    platformList.push(create.platformTagPS4(system))
-                } else if (system === 'PC') {
-                    platformList.push(create.platformTagPC(system))
-                } else { platformList.push(create.platformTagSwitch(system))}
-            }
-            // console.log(platformList);
-            const platformsFormatted = platformList.join('\n');
-            document.querySelector(`#platform-area[data-id="${game.igdb_id}"`).innerHTML = platformsFormatted; 
-        }
+    function formatDesc(str) {
+      if (str.length <= 300) {
+        return str
+      } else {
+        let newStr = str.slice(0, 300)
+        return `${newStr}...`;
+      }
     }
 
-    function addNewEvent() {
-        document.getElementById('add-new-game').addEventListener('click', function (e) {
-            e.preventDefault()
-            window.location = '/newlibgame.html'
-        })
+    // const headers = create.gameHeaders()
+
+    const appliedTemplates = arr.map(game => create.publicGameRow(game.igdb_id, formatCover(game.cover_url), game.title, formatDesc(game.desc))).join('\n');
+    // document.querySelector(".main-body").innerHTML = headers + appliedTemplates;
+    document.querySelector(".main-body").innerHTML = appliedTemplates;
+
+    for (let game of arr) {
+      const platformList = [];
+      for (let system of game.platforms) {
+        if (system === 'Xbox One') {
+          platformList.push(create.platformTagXbox(system))
+        } else if (system === 'Playstation 4') {
+          platformList.push(create.platformTagPS4(system))
+        } else if (system === 'PC') {
+          platformList.push(create.platformTagPC(system))
+        } else {
+          platformList.push(create.platformTagSwitch(system))
+        }
+      }
+      // console.log(platformList);
+      const platformsFormatted = platformList.join('\n');
+      document.querySelector(`#platform-area[data-id="${game.igdb_id}"`).innerHTML = platformsFormatted;
     }
-    addNewEvent()
-    getlibrary();
+  }
+
+  function addNewEvent() {
+    let link = '/newlibgame.html'
+    let linkID = window.location.search
+    let dynamicLink1 = link + linkID
+
+    document.getElementById('add-new-game').addEventListener('click', function (e) {
+      e.preventDefault()
+      window.location = dynamicLink1
+    })
+  }
+  addNewEvent()
+  getlibrary();
 }
 
 
-module.exports= {
-    init
+module.exports = {
+  init
 }
-
